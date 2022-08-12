27379dec1bcfb6a0f85662d9c71bdf8888e22c0e
Stanislav Volokhov <lamantina123@gmail.com>
2022-08-12 21:37:01 +0400

    Add Task23

--- Task#21/Program.cs
+++ Task#21/Program.cs
@@ -1,21 +1,21 @@
 ﻿// Задача 21
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-// Console.Clear();
-// Console.Write("Введите X1: ");
-// int x1=int.Parse(Console.ReadLine());
-// Console.Write("Введите Y1: ");
-// int y1=int.Parse(Console.ReadLine());
-// Console.Write("Введите Z1: ");
-// int z1=int.Parse(Console.ReadLine());
-// Console.Write("Введите X2: ");
-// int x2=int.Parse(Console.ReadLine());
-// Console.Write("Введите Y2: ");
-// int y2=int.Parse(Console.ReadLine());
-// Console.Write("Введите Z2: ");
-// int z2=int.Parse(Console.ReadLine());
+Console.Clear();
+Console.Write("Введите X1: ");
+int x1=int.Parse(Console.ReadLine()!);
+Console.Write("Введите Y1: ");
+int y1=int.Parse(Console.ReadLine()!);
+Console.Write("Введите Z1: ");
+int z1=int.Parse(Console.ReadLine()!);
+Console.Write("Введите X2: ");
+int x2=int.Parse(Console.ReadLine()!);
+Console.Write("Введите Y2: ");
+int y2=int.Parse(Console.ReadLine()!);
+Console.Write("Введите Z2: ");
+int z2=int.Parse(Console.ReadLine()!);
 
-// double d = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+double d = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
 
-// Console.WriteLine($"d={d:f2}");
+Console.WriteLine($"d={d:f2}");
 
