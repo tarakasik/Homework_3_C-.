f4e554671d0ce570552b50675d70a22b993308da
Stanislav Volokhov <lamantina123@gmail.com>
2022-08-13 17:59:47 +0400

    Did Homework

--- Task#23/Program.cs
+++ Task#23/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Input : ");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Input : ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 for(int i = 1; i <= n; i++)
